dcf3fc630325bc16219e69d4ec6d493170d7a760
prabhat giri <prabhat4133@gmail.com>
2022-09-28 11:38:43 +1000

    svg fix

--- dist/keyboard.js
+++ dist/keyboard.js
@@ -1,4 +1,4 @@
- // Plugin Items collection
+// Plugin Items collection
  var items = [
     // Text items
     {
@@ -6,8 +6,8 @@
         title: "1. Transfer of waste from Mo-99 production",
         description: "The ILW is stored and characterised prior to introduction to the waste treatment facility.",
         position: {
-            left: 1076,
-            top: 445
+            left: $('#first').offset().left,
+            top: $('#first').offset().top
         },
         picturePath: "./assets/synroc-step-graphic-1.jpg",
         customClassName: "synroc-diagram__hotspot visiblebox-1 first",
@@ -18,8 +18,8 @@
         title: "2. Addition of Synroc additives and drying",
         description: "The ILW is processed remotely within a hot cell and the tailored Synroc additives introduced and mixed with the waste. The liquid is converted into a granulated powder via a drying and calcination process.",
         position: {
-            left: 675,
-            top: 380
+            left: $('#second').offset().left,
+            top: $('#second').offset().top
         },
         picturePath: "./assets/synroc-step-graphic-2.jpg",
         customClassName: "synroc-diagram__hotspot visiblebox-2 second",
@@ -30,8 +30,8 @@
         title: "3. Filling the canister",
         description: "The granulated waste powder is dispensed into a specially designed canister.",
         position: {
-            left: 670,
-            top: 505
+            left: $('#third').offset().left,
+            top: $('#third').offset().top
         },
         picturePath: "./assets/synroc-step-graphic-3.jpg",
         customClassName: "synroc-diagram__hotspot visiblebox-3 third",
@@ -42,11 +42,11 @@
         title: "4. Hot isostatic pressing",
         description: "The canister is sealed and placed into a hot isostatic press where heat and pressure are applied to immobilise the waste.",
         position: {
-            left: 110,
-            top: 570
+            left: $('#fourth').offset().left,
+            top: $('#fourth').offset().top
         },
         picturePath: "./assets/synroc-step-graphic-4.jpg",
-        customClassName: "synroc-diagram__hotspot visiblebox-4 forth",
+        customClassName: "synroc-diagram__hotspot visiblebox-4 fourth",
         sticky: true
     },
     {
@@ -54,75 +54,10 @@
         title: "5. Durable compact wasteform",
         description: " The waste has now been treated into a durable, low-volume, product ready for final disposal and is transferred out of the waste treatment facility.",
         position: {
-            left: 392,
-            top: 385
+            left: $('#fifth').offset().left,
+            top: $('#fifth').offset().top
         },
         picturePath: "./assets/synroc-step-graphic-5.jpg",
-        customClassName: "synroc-diagram__hotspot visiblebox-5 fifth",
-        sticky: true
-    }
-];
-
-
-var mobile_items = [
-    // Text items
-    {
-        type: "text",
-        title: "1. Transfer of waste from Mo-99 production",
-        description: "The ILW is stored and characterised prior to introduction to the waste treatment facility.",
-        position: {
-            left: 803,
-            top: 327
-        },
-        picturePath: "./assets/synroc-step-graphic-mobile-1.jpg",
-        customClassName: "synroc-diagram__hotspot visiblebox-1 first",
-        sticky: true
-    },
-    {
-        type: "text",
-        title: "2. Addition of Synroc additives and drying",
-        description: "The ILW is processed remotely within a hot cell and the tailored Synroc additives introduced and mixed with the waste. The liquid is converted into a granulated powder via a drying and calcination process.",
-        position: {
-            left: 500,
-            top: 277
-        },
-        picturePath: "./assets/synroc-step-graphic-mobile-2.jpg",
-        customClassName: "synroc-diagram__hotspot visiblebox-2 second",
-        sticky: true
-    },
-    {
-        type: "text",
-        title: "3. Filling the canister",
-        description: "The granulated waste powder is dispensed into a specially designed canister.",
-        position: {
-            left: 500,
-            top: 375
-        },
-        picturePath: "./assets/synroc-step-graphic-mobile-3.jpg",
-        customClassName: "synroc-diagram__hotspot visiblebox-3 third",
-        sticky: true
-    },
-    {
-        type: "text",
-        title: "4. Hot isostatic pressing",
-        description: "The canister is sealed and placed into a hot isostatic press where heat and pressure are applied to immobilise the waste.",
-        position: {
-            left: 85,
-            top: 430
-        },
-        picturePath: "./assets/synroc-step-graphic-mobile-4.jpg",
-        customClassName: "synroc-diagram__hotspot visiblebox-4 forth",
-        sticky: true
-    },
-    {
-        type: "text",
-        title: "5. Durable compact wasteform",
-        description: " The waste has now been treated into a durable, low-volume, product ready for final disposal and is transferred out of the waste treatment facility.",
-        position: {
-            left: 290,
-            top: 285
-        },
-        picturePath: "./assets/synroc-step-graphic-mobile-5.jpg",
         customClassName: "synroc-diagram__hotspot visiblebox-5 fifth",
         sticky: true
     }
@@ -138,65 +73,83 @@ var options = {
 
 // Plugin activation
 $(document).ready(function() {
-    if(window.outerWidth <=767) {
-        $("#synroc-diagram-mobile").interactiveImage(mobile_items, options);
-        $("#synroc-diagram-desktop").css('display', 'none');
-        $("#synroc-diagram-mobile").css('display', 'block');
-    }
-    else{
-        $("#synroc-diagram-desktop").interactiveImage(items, options);
-        $("#synroc-diagram-mobile").css('display', 'none');
-        $("#synroc-diagram-desktop").css('display', 'block');
-    }
+    $("#synroc-diagram").interactiveImage(items, options);    
+    setTimeout(function() {
+            reScale();
+            reAlign();
+            closeOthers(); 
+    }, 100);
+    
 });
 
 function reScale(){
-    // $('div#synroc-diagram-desktop').css('transform-origin', 'top left');
-    // $('div#synroc-diagram-desktop').css('transform', 'scale(' + ((window.parent.outerWidth/1280) /2 ) + ')');
-    if(window.parent.outerWidth > 900 && window.parent.outerWidth < 1280) {
-        $('div#synroc-diagram-desktop').css('transform-origin', 'top left');
-        $('div#synroc-diagram-desktop').css('transform', 'scale(' + (((window.parent.outerWidth/1280) ) / 1.3 ) + ')');
-    }
-    if(window.parent.outerWidth > 767 && window.parent.outerWidth < 830) {
-        $('div#synroc-diagram-desktop').css('transform-origin', 'top left');
-        $('div#synroc-diagram-desktop').css('transform', 'scale(' + ((window.parent.outerWidth/1280) / 1.2 ) + ')');
-    }
-    if(window.parent.outerWidth > 830 && window.parent.outerWidth < 900) {
-        $('div#synroc-diagram-desktop').css('transform-origin', 'top left');
-        $('div#synroc-diagram-desktop').css('transform', 'scale(' + ((window.parent.outerWidth/1280)  / 1.3 ) + ')');
-    }
+    console.log('here');
+
+   
+    $('#first').attr('data-for',$('.first').prev().attr('data-for'));
+    $('#first').attr('class',$('.first').prev().attr('class'));
+    $('.first').prev().remove();
+
+    $('#second').attr('data-for',$('.second').prev().attr('data-for'));
+    $('#second').attr('class',$('.second').prev().attr('class'));
+    $('.second').prev().remove();
+    
+    $('#third').attr('data-for',$('.third').prev().attr('data-for'));
+    $('#third').attr('class',$('.third').prev().attr('class'));
+    $('.third').prev().remove();
+
+    $('#fourth').attr('data-for',$('.fourth').prev().attr('data-for'));
+    $('#fourth').attr('class',$('.fourth').prev().attr('class'));
+    $('.fourth').prev().remove();
+
+    $('#fifth').attr('data-for',$('.fifth').prev().attr('data-for'));
+    $('#fifth').attr('class',$('.fifth').prev().attr('class'));
+    $('.fifth').prev().remove();
 }
-$(parent).on('load', function(){
-    reScale();
-});
 
+function reAlign(){
+    $('.first').css('left', $('#first').offset().left-400);
+    $('.first').css('top', $('#first').offset().top-100);
 
-$(parent).on('resize', function(){
-    console.log($(parent).closest(' .interactive-flow-diagram__iframe'));
-    reScale();
-    if(window.outerWidth <=767) {
-        if(!$('#synroc-diagram-mobile .hotspot').length >= 1) {
-            $("#synroc-diagram-mobile").interactiveImage(mobile_items, options);
+    $('.second').css('left', $('#second').offset().left-400);
+    $('.second').css('top', $('#second').offset().top-100);
+
+    $('.third').css('left', $('#third').offset().left-400);
+    $('.third').css('top', $('#third').offset().top-100);
+
+    $('.fourth').css('left', $('#fourth').offset().left+100);
+    $('.fourth').css('top', $('#fourth').offset().top-200);
+
+    $('.fifth').css('left', $('#fifth').offset().left-400);
+    $('.fifth').css('top', $('#fifth').offset().top-100);
+}
+
+$(parent).on('load, resize', function(){
+    setTimeout(function() {
+        if(window.outerWidth <=767) {
+            if(!$('#synroc-diagram .hotspot').length >= 1) {
+                $("#synroc-diagram").interactiveImage(items, options);
+                closeOthers();
+            }
+            if(!$('#synroc-diagram .visiblebox-1 .text-item .navigation').length >=1 ) {
+                $('#synroc-diagram .text-item').append($('#synroc-diagram .navigation'));
+            }
+        }
+        else{
+            
+            reAlign(); 
+            if($('#synroc-diagram .hotspot').length <=1) {
+                $("#synroc-diagram").interactiveImage(items, options);
+            }
+            if($('#synroc-diagram .navigation').length <=5) {
+                $('#synroc-diagram').append($('#synroc-diagram .visiblebox-1 .text-item .navigation').clone());
+                $('#synroc-diagram .text-item .navigation').remove();
+            }
             closeOthers();
         }
-        if(!$('#synroc-diagram-mobile .visiblebox-1 .text-item .navigation').length >=1 ) {
-            $('#synroc-diagram-mobile .text-item').append($('#synroc-diagram-desktop .navigation'));
-        }
-        $("#synroc-diagram-desktop").css('display', 'none');
-        $("#synroc-diagram-mobile").css('display', 'block');
-    }
-    else{
-        if($('#synroc-diagram-desktop .hotspot').length <=1) {
-            $("#synroc-diagram-desktop").interactiveImage(items, options);
-        }
-        if($('#synroc-diagram-desktop .navigation').length <=1) {
-            $('#synroc-diagram-desktop').append($('#synroc-diagram-mobile .visiblebox-1 .text-item .navigation').clone());
-        }
-        closeOthers();
-        $("#synroc-diagram-mobile").css('display', 'none');
-        $("#synroc-diagram-desktop").css('display', 'block');
-    }
+    }, 100);
 });
+
 
 $(document).ready(function() {
     $('body').on('click', '.close-button', function () {
@@ -204,10 +157,6 @@ $(document).ready(function() {
     })
 
     closeOthers();
-
-    if(window.outerWidth <=767) {
-        $('#synroc-diagram-mobile .text-item').append($('.navigation'));
-    }
 });
 
 $("body").on('keydown', function(e) {
@@ -222,7 +171,7 @@ $("body").on('keydown', function(e) {
                 moveRight(classNo, thiss);
           }   
     }else{
-        if(e.keyCode == 37) {
+        if(e.keyCode <= 40 && e.keyCode >=37) {
             displayDefault()
         }
     }    
@@ -257,7 +206,8 @@ $(".left, .right").on('click', function(e) {
 function displayDefault(){
     $('.visiblebox-1').css('display', 'block');  
     $('.visiblebox-1').addClass('active');
-    $('.visiblebox-1').prev('.icon-radio-checked').addClass('selected');
+    var active = $('.icon-radio-checked')[0];
+    $(active).addClass('selected');    
 }
 function moveLeft(classNo, thiss){
     classNo = classNo + 1;
@@ -274,11 +224,13 @@ function moveRight(classNo, thiss){
 function displayActive(classNo, thiss){
     cls = ".visiblebox-" + classNo;
     thiss.css('display', 'none');
-    thiss.prev('.icon-radio-checked').removeClass('selected');
+    $('.icon-radio-checked').removeClass('selected');
     $(cls).css('display', 'block');  
     thiss.removeClass('active');
     $(cls).addClass('active');
-    $(cls).prev('.icon-radio-checked').addClass('selected');
+    console.log(classNo);
+    var active = $('.icon-radio-checked')[classNo - 1];
+    $(active).addClass('selected');
 }
 
 function closeOthers(){
